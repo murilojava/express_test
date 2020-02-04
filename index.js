c7079dd21fb68636b26eb6d174daef2f6fc9d88b
Murilo Barbosam <murilojava@gmail.com>
2020-02-04 17:44:34 -0300

    Ajuste de CORS

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
 
 const products = [
     {id:1, name:"teste1", value:10.25},
-    {id:2, name:"teste2", value:29.25},
+    {id:2, name:"teste2", value:29.25, price:14.8, type:"teste"},
     {id:3, name:"teste3", value:30.11},
 ]
 
+app.use(cors())
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
